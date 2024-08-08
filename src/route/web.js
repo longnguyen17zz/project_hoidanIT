32d725545f616a11a928f6de4e8254f133485b71
longnguyen17zz <longhq17@gmail.com>
2024-08-08 17:54:44 +0700

    connect-database

--- src/route/web.js
+++ src/route/web.js
@@ -1,9 +1,14 @@
 import express from "express";
 
+import homeController from "../controller/homeController";
+
 let router = express.Router();
 let initWebRoutes = (app) => {
-  router.get("/", (req, res) => {
-    return res.send("Welcome");
+  // rest API
+  router.get("/", homeController.getHomePage);
+
+  router.get("/longhq", (req, res) => {
+    return res.send("Welcome with LongHQ");
   });
 
   return app.use("/", router);
