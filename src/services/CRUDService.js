5831ddbeed1072c17fd608d3a33f8e1400924b4a
longnguyen17zz <longhq17@gmail.com>
2024-08-14 14:24:17 +0700

    create-user-fix-password

--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -9,7 +9,7 @@ let createNewUser = async (data) => {
       let hashPasswordFromBcrypt = await hashUserPassword(data.password);
       await db.User.create({
         email: data.email,
-        password: data.hashPasswordFromBcrypt,
+        password: hashPasswordFromBcrypt,
         firstName: data.firstName,
         lastName: data.lastName,
         address: data.address,
